bf23e212ff94ffc3910575581956542d0be56dd4
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-02-11 09:42:19 -0300

    feat: adição de título da página de post dinamicamente

--- src/app/components/artigo/artigo.component.ts
+++ src/app/components/artigo/artigo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 import { BlogPost } from 'src/app/models/blog-post';
 import { BlogService } from 'src/app/services/blog.service';
 
@@ -13,12 +14,12 @@ export class ArtigoComponent implements OnInit{
   ngOnInit(): void {
     const postId = this.activatedRoute.snapshot.params['id'];
     this.blogService.getPostById(postId).subscribe({
-      next: res => {this.post = res.data.post; console.log(res)},
+      next: res => {this.post = res.data.post; this.title.setTitle(this.post?.titulo + ' | Blog Bushido')},
       error: error => console.log(error)
     })
   }
 
-  constructor(private blogService: BlogService) {}
+  constructor(private blogService: BlogService, private title: Title) {}
 
   public post: BlogPost | undefined;
 
