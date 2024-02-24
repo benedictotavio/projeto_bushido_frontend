7ef1077a8ea040b2b4d4b8479fe02d5671f98cbe
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-02-24 16:43:45 -0300

    add beatriz voluntary, add rotas nos botões

--- src/app/components/whatsapp-button/whatsapp-button.component.ts
+++ src/app/components/whatsapp-button/whatsapp-button.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core'
   templateUrl: './whatsapp-button.component.html',
   styleUrls: ['./whatsapp-button.component.css'],
 })
-export class WhatsappButtonComponent {}
+export class WhatsappButtonComponent {
+  
+}
