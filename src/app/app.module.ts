65153bfe66c1bd5ee585e26c8e5f8774a18482db
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-03-10 16:40:02 -0300

    httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
@@ -64,10 +64,9 @@ import { ArtigoComponent } from './components/artigo/artigo.component'
     AdminComponent,
     DashboardComponent,
     BlogCardComponent,
-    ArtigoComponent
+    ArtigoComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule]
-  ,
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
