f816bb2cb5b9d880b576b30f34e0c08f0802394d
beneditotavio <otavio.bene@outlook.com>
2024-03-20 01:00:29 -0300

    feature: criação de rota para criação de aluno

---
+++ src/app/pages/admin/registro-aluno/registro-aluno.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-aluno',
+  templateUrl: './registro-aluno.component.html',
+  styleUrls: ['./registro-aluno.component.css']
+})
+export class RegistroAlunoComponent {
+  
+}
