a104856128cf3530b665f23b0591ecc3586f9a60
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-03-20 15:13:56 -0300

    Alterações environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
+
 export const environment = {
-  datoToken: '7176c17699c1a61a100c9a7d35d69a',
+  production: false,
+  datoToken:  '7176c17699c1a61a100c9a7d35d69a',
   urlApi: 'https://projeto-bushido-backend.onrender.com/api/V1',
 }
