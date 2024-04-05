794ce2d40ec4b95c53e4cb861ac476fa709bb7bb
beneditotavio <otavio.bene@outlook.com>
2024-04-05 01:44:48 -0300

    feature: sessão de graduaçao

--- src/app/components/modal/modal-historico-graduacao/modal-historico-graduacao.component.ts
+++ src/app/components/modal/modal-historico-graduacao/modal-historico-graduacao.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { Graduacao } from 'src/app/pages/admin/aluno.interface'
 
 @Component({
   selector: 'app-modal-historico-graduacao',
   templateUrl: './modal-historico-graduacao.component.html',
   styleUrls: ['./modal-historico-graduacao.component.css'],
 })
+export class ModalHistoricoGraduacaoComponent {
+  @Input()
+  graduacao!: Graduacao[]
+  constructor() {}
 
-// TODO: Implementar o componente para ver o historico de Graduacao
-export class ModalHistoricoGraduacaoComponent {}
+  protected graduacaoFinal(index: number) {
+    return index === this.graduacao.length - 1
+  }
+}
--- src/app/pages/admin/aluno.interface.ts
+++ src/app/pages/admin/aluno.interface.ts
@@ -1,67 +1,93 @@
+interface Endereco {
+  cidade: string;
+  estado: string;
+  cep: string;
+  numero: string;
+}
+
 interface Responsavel {
-  nome: string
-  cpf: string
-  telefone: string
-  email: string
-  filiacao: string
+  nome: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  filiacao: string;
+}
+
+interface Faltas {
+  data: string;
+  motivo: string;
+  observacao: string;
+}
+
+export interface Graduacao {
+  kyu: number;
+  faltas: Faltas[];
+  status: boolean;
+  inicioGraduacao: string;
+  fimGraduacao: string;
+  frequencia: number;
+  aprovado: boolean;
+  cargaHoraria: number;
+  dan: number;
 }
 
 interface UsoMedicamento {
-  resposta: boolean
-  tipo: string
+  resposta: boolean;
+  tipo: string;
+}
+
+interface Alergia {
+  resposta: boolean;
+  tipo: string;
+}
+
+interface Cirurgia {
+  resposta: boolean;
+  tipo: string;
+}
+
+interface DoencaCronica {
+  resposta: boolean;
+  tipo: string;
 }
 
 interface HistoricoSaude {
-  fatorRh: string
-  tipoSanguineo: string
-  usoMedicamento: UsoMedicamento
-  alergia: { resposta: boolean; tipo: string }
-  cirurgia: { resposta: boolean; tipo: string }
-  doencaCronica: { resposta: boolean; tipo: string }
-  deficiencias: string[]
-  acompanhamentoSaude: string[]
+  fatorRh: string;
+  tipoSanguineo: string;
+  usoMedicamento: UsoMedicamento;
+  alergia: Alergia;
+  cirurgia: Cirurgia;
+  doencaCronica: DoencaCronica;
+  deficiencias: any[]; // Define the type of deficiencias as needed
+  acompanhamentoSaude: any[]; // Define the type of acompanhamentoSaude as needed
 }
 
 interface DadosSociais {
-  bolsaFamilia: boolean
-  auxilioBrasil: boolean
-  imovel: string
-  numerosDePessoasNaCasa: number
-  contribuintesDaRendaFamiliar: number
-  alunoContribuiParaRenda: boolean
-  rendaFamiliarEmSalariosMinimos: number
+  bolsaFamilia: boolean;
+  auxilioBrasil: boolean;
+  imovel: string;
+  numerosDePessoasNaCasa: number;
+  contribuintesDaRendaFamiliar: number;
+  alunoContribuiParaRenda: boolean;
+  rendaFamiliarEmSalariosMinimos: number;
 }
 
 interface DadosEscolares {
-  turno: string
-  escola: string
-  serie: string
-}
-
-interface Graduacao {
-  kyu: number
-  faltas: any[]
-  status: boolean
-  frequencia: number
-}
-
-interface Endereco {
-  cidade: string
-  estado: string
-  cep: string
-  numero: string
+  turno: string;
+  escola: string;
+  serie: string;
 }
 
 export interface AlunoResponse {
-  nome: string
-  dataNascimento: string
-  genero: string
-  dadosSociais: DadosSociais
-  dadosEscolares: DadosEscolares
-  dataPreenchimento: string
-  endereco: Endereco
-  rg: string
-  responsaveis: Responsavel[]
-  graduacao: Graduacao
-  historicoSaude: HistoricoSaude
+  nome: string;
+  dataNascimento: string;
+  genero: string;
+  dadosSociais: DadosSociais;
+  dadosEscolares: DadosEscolares;
+  dataPreenchimento: string;
+  endereco: Endereco;
+  rg: string;
+  responsaveis: Responsavel[];
+  graduacao: Graduacao[];
+  historicoSaude: HistoricoSaude;
 }
